30ccabd75fc48c15b44ae51663f9493b8329da17
RNA <a@b.com>
2025-06-10 15:03:35 +0200

    toggle

--- script.js
+++ script.js
@@ -3,28 +3,20 @@ const btnClair = document.getElementById('btn-clair');
 const themeActuel = document.getElementById('theme-actuel');
 
 
-btnSombre.onclick = function (){
-    document.body.classList.add ('theme-sombre');
-    document.body.classList.remove ('theme-clair');
-    themeActuel.textContent = 'Sombre';
-}
 
 btnSombre.onclick = function (){
     btnSombre.textContent = '🌙 Activé !';
-    document.body.classList.add ('theme-sombre');
+    document.body.classList.toggle ('theme-sombre');
     document.body.classList.remove ('theme-clair');
     themeActuel.textContent = 'Sombre';
     btnClair.textContent = '☀️ Thème Clair';
 }
 
 btnClair.addEventListener ('click', function () {
+    btnClair.textContent = '☀️Activé';
     document.body.classList.add ('theme-clair');
     document.body.classList.remove ('theme-sombre');
     themeActuel.textContent = 'Clair';
+    btnSombre.textContent = '🌙 Thème Sombre';
 })
 
-btnClair.addEventListener ('click', function () {
-    btnClair.textContent = '☀️Activé';
-    btnSombre.textContent = '🌙 Thème Sombre';
-    console.log('Thème clair activé !');
-})
